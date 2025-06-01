54dc0be2d8f374ca0df7d5a2b62318ec96c89433
Dyzer4 <dyegodasilvaroque86@gmail.com>
2025-06-01 16:26:03 -0300

    feat: login final

---
+++ src/script/login.js
@@ -0,0 +1,33 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value.trim();
+  const senha = document.getElementById('senha').value.trim();
+
+  try {
+    const response = await fetch(`https://apichecksaude-dmcqhmgcdwcnehez.centralus-01.azurewebsites.net/api/login?email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}`, {
+      method: 'GET'
+    });
+
+    if (!response.ok) {
+      throw new Error('Erro ao conectar com o servidor');
+    }
+
+    const usuario = await response.json();
+    console.log(usuario)
+
+    if (usuario && usuario.id) {
+      localStorage.setItem('usuarioId', usuario.id);
+      window.location.href = './src/pages/principal';
+    } else {
+      document.getElementById('mensagem').textContent = 'E-mail ou senha incorretos!';
+    }
+  } catch (error) {
+    document.getElementById('mensagem').textContent = 'Erro ao conectar com o servidor!';
+    console.error(error);
+  }
+
+  const usuarioId = localStorage.getItem('usuarioId');
+});
+
+  export { usuarioId }
